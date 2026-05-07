3617d8c3a496866508bdf3d75e62473f77f0767d
fledwings <fledwings9@gmail.com>
2026-05-06 21:01:53 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-{
-  "imports": {
-    "@supabase/functions-js": "jsr:@supabase/functions-js@^2"
-  }
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
